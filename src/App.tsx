1cb494c728d9507d68379c75edc7d95e4fbd3622
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2023-03-06 13:11:37 +0200

    custom render

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import { Application } from "./components/Application/Applicaton";
+import "./App.css";
+// import { Application } from "./components/application/Application";
+// import { CounterTwo } from "./components/counter-two/CounterTwo";
+// import { Counter } from "./components/counter/counter";
+import { MuiMode } from "./components/mui/mui-mode";
+// import { Skills } from "./components/skills/Skills";
+// import { Users } from "./components/users/Users";
+import { AppProviders } from "./providers/app-providers";
 function App() {
-  return <Application />;
+  return (
+    <AppProviders>
+      <div className="App">
+        {/* <Application /> */}
+        {/* <Skills skills={["HTML", "CSS"]} /> */}
+        {/* <Counter /> */}
+        {/* <CounterTwo count={1} /> */}
+        {/* <Users /> */}
+        <MuiMode />
+      </div>
+    </AppProviders>
+  );
 }
 
 export default App;
